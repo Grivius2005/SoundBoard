add74acbe0d91d0a5ad808328406346456e6ef5f
Grzegorz Wiśniewski <grzegorz2005.gwn@gmail.com>
2026-02-10 18:56:01 +0100

    - Created wpf app - Add icon - Add grid layout with some boxes and buttons - Add part of device loading method

---
+++ SoundBoard/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio.Wave;
+using SBClassLib;
+
+namespace SoundBoard
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SoundManager _manager;
+        public string DirectoryPath { get; set; }
+        public ObservableCollection<DirectSoundDeviceInfo> SoundOutDevices { get; set; }
+
+        public MainWindow()
+        {
+
+            _manager = new SoundManager();
+            InitializeComponent();
+            this.DataContext = this;
+            LoadDevices();
+        }
+
+
+
+        private void LoadDevices()
+        {
+            SoundOutDevices = new ObservableCollection<DirectSoundDeviceInfo>(SoundManager.GetOutDevices());
+            FirstDeviceCbox.ItemsSource = SoundOutDevices;
+            SecondDeviceCbox.ItemsSource = SoundOutDevices;
+
+
+
+        }
+    }
+}
